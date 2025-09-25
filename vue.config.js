6e2a94aab61a8a26b6be7aced5f2a2f4c34ac69c
Aleksandr Zhadanov <33748120+Alex119010@users.noreply.github.com>
2025-09-25 21:43:11 +0300

    13

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-
 // const { defineConfig } = require('@vue/cli-service')
-
 // module.exports = defineConfig({
-//   transpileDependencies: true,
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/hello-world/' 
-//     : '/'
+//   transpileDependencies: true
 // })
+
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/project' 
+    : '/'
+})
 
